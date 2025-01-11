13856dd59c78658a8262524fad76ae922b3574f8
safoukou <safoukouc@gmail.com>
2025-01-11 09:49:47 +0200

    Datatypes class

---
+++ python101/python01/datatype.py
@@ -0,0 +1,37 @@
+print("hello wolrd")
+print(42) #integer
+print(3.14) #float
+print(True) #boolean
+print(None) #NoneType
+print([])
+print(())
+print({})
+
+ages = [23, 45, 56, 78] #list
+print(ages)
+print(f"the type of {ages} is {type(ages)}")
+user = {"Name": "Calvin", "age": 18, "Address": "Florida, Mzanzi", "Gender": "Male"} #dictionary
+print(user)
+
+#reading datatypes
+print (f"the ages of the first person is: {ages[0]} years old")
+print (f"the second person is: {ages[1]} years old")
+
+print(user["Name"]) #slicingg
+print("range(10)") #range
+
+
+greet = 'Hello World!'
+introduce = greet+f'\nMy name is {user["Name"]} and I am {user["Gender"]}' #string concatenation
+print(introduce)
+print{{"Calvin", 23}}
+user =(user["name"], users["age"]) #Tuple
+print(user)
+print(user[1]) #indexing
+print(user)
+
+#boolean
+"""Author: joel
+   date: 2025-01-11
+   decription"""
+

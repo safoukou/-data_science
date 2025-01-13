002ff37ab9e700389138312884fddd869e8af1cb
safoukou <safoukouc@gmail.com>
2025-01-13 05:46:20 +0200

    Functions, dictionaries and list manipulation

--- python101/python01/datatype.py
+++ python101/python01/datatype.py
@@ -7,10 +7,11 @@ print([])
 print(())
 print({})
 
+
 ages = [23, 45, 56, 78] #list
 print(ages)
 print(f"the type of {ages} is {type(ages)}")
-user = {"Name": "Calvin", "age": 18, "Address": "Florida, Mzanzi", "Gender": "Male"} #dictionary
+user = {"Name": "Calvin", "age": 18, "Address": "Florida, Mzanzi", "Gender": "Male"} #dictionary 
 print(user)
 
 #reading datatypes
@@ -22,16 +23,41 @@ print("range(10)") #range
 
 
 greet = 'Hello World!'
+name = "calvin"
 introduce = greet+f'\nMy name is {user["Name"]} and I am {user["Gender"]}' #string concatenation
 print(introduce)
-print{{"Calvin", 23}}
-user =(user["name"], users["age"]) #Tuple
+print({"Calvin", 23})
+#user =(user["name"], user["age"]) #Tuple
 print(user)
-print(user[1]) #indexing
+#print(user[1]) #indexing
 print(user)
 
 #boolean
+
 """Author: joel
    date: 2025-01-11
-   decription"""
+   decription: Thisis a multiline comment
+   License: MIT
+   """
+is_adult = False
+
+if user["age"] >= 18:
+    is_adult = True  #constant name "is_adult" doesn't conform to upper_case namin style
+    print("you are an adult")
+else:
+    print("you are a minor")
+
+print(is_adult)
+
+"""check if the user is a minor or an adult"""  #string statment has no effect
+if user["age"] < 18:
+    print("you are a minor")
+else:
+    is_adult = True
+    print("you are an adult")
+
+if is_adult:
+    print("you are allowed to drink alcohol")
+else:
+    print("you arre not allowed to drik alcohol")
 
---
+++ python101/python02/function.py
@@ -0,0 +1,81 @@
+introduce = "Hello World"
+
+introduce = introduce.upper() #string methods
+print(introduce)
+
+def say_hi(first_name, last_name):
+    name =first_name + '' + last_name
+    return f"Hello {name}!"
+
+
+print(say_hi("Calvin", "Safoukou" .capitalize())) #Fontion call
+print(say_hi("Toure".upper(), "Bonaberi".lower()))
+
+
+def add(a,b):
+    return a + b
+
+x = 12.5
+y = 14.8
+print(f"The sum of {x} and {y} is {add(x,y)}")
+
+def divide(a,b):
+    if b>0:
+        if a==0:
+            return "Zero divide by any number is zero"
+        return a/b
+    else:
+        return "Division by Zero is not allowed"
+
+def divide2(a,b):
+    result = a/b if b>0 else "divisionby zero is not allowed"
+    return result
+
+def is_float(num, name):
+    if (type(num)==float) or name== 'joel':
+        return True
+    return False
+
+
+
+
+
+def divide2(a,b):
+    result = a/b if b>0 else "division by zero is not allowed"
+    return result
+    
+def is_float(num, name):
+    if (type(num)==float) or name=="joel":
+        return True
+        return False
+    
+print(abs(round(divide2(-4,58), 3)))
+
+print(is_float(divide2(9,2), "herve"))
+
+
+student_name =["Calvin", "Jules", "joel"]
+student_ages =[23, 45, 56, 76, 89] 
+
+for index, first_name in enumerate(student_name):
+    age =student_ages[index]
+    print(f"{first_name} is {age} years old")
+    
+    
+cities =[{"Douala": "Littoral", "yaoude": "Centre"}, {"Johannesburg": "Gauteng"}, 
+        {"Nairobi": "Kenya"}]
+cities2 = {"Douala": "Littoral", "Yaounde": "Centre", "Bafoussam": "West", "Bamenda": "North West", "Buea": "South West", 
+               "Garoua": "North", "Maroua": "Far North", "ngaoundere": "Adamawa",
+               "Ebolowa": "South", "Kribi": "South", "Limbe": "South West"}
+print(cities[0]["Douala"])
+print(cities2["Douala"])
+
+for key in cities:
+    print(key)
+
+for city in cities2.values():
+    print(city)
+
+
+
+

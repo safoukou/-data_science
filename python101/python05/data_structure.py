40bf15de83702c9bdeef31e55e4e7a7c4942394f
safoukou <safoukouc@gmail.com>
2025-01-15 08:29:38 +0200

    dictionnaries and data list

--- python101/python05/data_structure.py
+++ python101/python05/data_structure.py
@@ -12,3 +12,17 @@ for name in employees_names:
         print(F"hELLO {name}, you are doing great")
     else: 
         print("Hello", name)
+
+
+#dictionary and list
+employees ={"Jules":[3000, "Data scientist", "german"], 
+           "joel":[5000, "machine learning ingenieer", "german"],
+           "calvin":[30000, "data engineer", "usa"]}
+print(employees["Jules"])
+
+for name, employees_info in employees.items():
+    print(f"Name: {name}")
+    print(f"salary: {employees_info[0]}")
+    print(f"Role: {employees_info[1]}")
+    print(f"country: {employees_info[-1]}")
+
